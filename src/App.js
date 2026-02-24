904900abeaaeb414cecb3551ecd9c3943fbc2e97
euler.amorim <euler.amorim@hotmail.com>
2026-02-24 10:26:38 -0300

    aula 02 - Entendendo o JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Joao';
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = 'https://via.placeholder.com/150';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>      
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(2, 2)}</p>
+      <img src={url} alt="Minha imagem" />
     </div>
   );
 }
